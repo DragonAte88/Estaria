f8a980ae8282f0f2df9a6c62c32b0787aef70200
DragonAte88 <clyricpenny@gmail.com>
2025-07-11 16:18:51 -0700

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./*.html'], // This tells Tailwind to scan all HTML files in the root directory
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
